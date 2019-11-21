098a406a547a9c4ea62120563cf7e8ce094ed1f9
CateNjeri01 <36324504+CateNjeri01@users.noreply.github.com>
2019-11-21 10:15:46 +0300

    Add files via upload

---
+++ frm_Initial.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_Initial
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_decryptedtext = New System.Windows.Forms.TextBox()
+        Me.btn_save = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_encryptedText = New System.Windows.Forms.TextBox()
+        Me.btn_encrypt = New System.Windows.Forms.Button()
+        Me.txt_plainTest = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(174, 171)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(190, 34)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Decrypt"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(38, 243)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Plain Text:"
+        '
+        'txt_decryptedtext
+        '
+        Me.txt_decryptedtext.Location = New System.Drawing.Point(174, 243)
+        Me.txt_decryptedtext.Name = "txt_decryptedtext"
+        Me.txt_decryptedtext.Size = New System.Drawing.Size(196, 20)
+        Me.txt_decryptedtext.TabIndex = 15
+        '
+        'btn_save
+        '
+        Me.btn_save.Location = New System.Drawing.Point(393, 94)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(111, 99)
+        Me.btn_save.TabIndex = 14
+        Me.btn_save.Text = "Save"
+        Me.btn_save.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 135)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Cipher Text:"
+        '
+        'txt_encryptedText
+        '
+        Me.txt_encryptedText.Location = New System.Drawing.Point(174, 129)
+        Me.txt_encryptedText.Name = "txt_encryptedText"
+        Me.txt_encryptedText.Size = New System.Drawing.Size(196, 20)
+        Me.txt_encryptedText.TabIndex = 12
+        '
+        'btn_encrypt
+        '
+        Me.btn_encrypt.Location = New System.Drawing.Point(174, 63)
+        Me.btn_encrypt.Name = "btn_encrypt"
+        Me.btn_encrypt.Size = New System.Drawing.Size(196, 36)
+        Me.btn_encrypt.TabIndex = 11
+        Me.btn_encrypt.Text = "Encrypt"
+        Me.btn_encrypt.UseVisualStyleBackColor = True
+        '
+        'txt_plainTest
+        '
+        Me.txt_plainTest.Location = New System.Drawing.Point(174, 25)
+        Me.txt_plainTest.Name = "txt_plainTest"
+        Me.txt_plainTest.Size = New System.Drawing.Size(196, 20)
+        Me.txt_plainTest.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Enter Text:"
+        '
+        'frm_Initial
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(532, 314)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txt_decryptedtext)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt_encryptedText)
+        Me.Controls.Add(Me.btn_encrypt)
+        Me.Controls.Add(Me.txt_plainTest)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frm_Initial"
+        Me.Text = "Encryption"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt_decryptedtext As TextBox
+    Friend WithEvents btn_save As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_encryptedText As TextBox
+    Friend WithEvents btn_encrypt As Button
+    Friend WithEvents txt_plainTest As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ frm_Initial.vb
@@ -0,0 +1,221 @@
+﻿Imports System.Data.SqlClient
+Imports System.Diagnostics
+Imports System.Timers
+Imports System.Runtime.InteropServices
+Imports System.IO
+Imports System.Data.OleDb
+Public Class frm_Initial
+    Dim conncheck As Boolean = False
+
+    Dim con As New SqlConnection
+
+    Dim con_oledb As New OleDbConnection
+
+    Dim com_oledb As New OleDbCommand
+
+    Dim com As New SqlCommand
+
+    Dim DA As SqlDataAdapter
+
+    Dim DS As DataSet
+
+    Dim insert_DR As SqlDataReader
+
+    Dim oledb_insert_DR As OleDbDataReader
+
+    Dim constr As String = initialnetworktester()
+
+    Dim user_id As Integer = 1
+
+    Dim connection As String = "Provider=Microsoft.Jet.OleDB.4.0; Data Source=|DataDirectory|\infinitum.mdb; Jet OLEDB:Database Password=kagumo;"
+
+    Private Sub frm_Initial_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    'Getting the mdf connection using OLEDB
+    Sub oledbdbConnect(ByVal comstr As String)
+
+        Try
+
+            con_oledb = New OleDbConnection(connection)
+            com_oledb = New OleDbCommand(comstr, con_oledb)
+
+        Catch ex As Exception
+
+            Dim error_log As String = "Error retrieving oledb:::   " & ex.Message
+
+        End Try
+
+    End Sub
+    'Getting sql connection 
+    Sub dbConnect(ByVal comstr As String)
+
+        Try
+            Dim constr As String = initialnetworktester()
+
+            con = New SqlConnection(constr)
+
+            com = New SqlCommand(comstr, con)
+
+        Catch ex As Exception
+
+            Dim error_log As String = "Error sql  connector:::   " & ex.Message
+
+        End Try
+
+    End Sub
+
+    Dim s_name, u_name, pass, catalog, serverconstring As String
+    'setting sql connection from the infinitum mdf
+    Public Sub setconnector()
+
+        Try
+            Dim id As String = "1"
+
+            Dim m_com_string As String = "select server_name, user_name, user_password, initial_catalog from tblconn where ID =" + "'" + id + "'" + ""
+
+            oledbdbConnect(m_com_string)
+
+            con_oledb.Open()
+
+            oledb_insert_DR = com_oledb.ExecuteReader
+
+            While oledb_insert_DR.Read()
+
+                s_name = oledb_insert_DR.GetValue(0).ToString
+
+                u_name = oledb_insert_DR.GetValue(1).ToString
+
+                pass = oledb_insert_DR.GetValue(2).ToString
+
+                catalog = oledb_insert_DR.GetValue(3).ToString
+
+                serverconstring = "Data Source=" + s_name + ";Initial Catalog=" + catalog + ";Persist Security Info=True;User ID=" + u_name + "; Password=" + pass + ""
+
+                ' mic_events_logger(serverconstring)
+
+            End While
+
+            oledb_insert_DR.Close()
+
+            con_oledb.Close()
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+    'getting the initial connector of the database
+    Dim retrievedconnector As String
+    Dim testednetworkconnector As String
+    Public Function initialnetworktester()
+
+        Try
+            setconnector()
+
+            retrievedconnector = serverconstring
+
+            Using sqlconnector As New SqlConnection(retrievedconnector)
+
+                sqlconnector.Open()
+                Dim connectionstatestate As String = sqlconnector.State.ToString
+                If connectionstatestate = "Open" Then
+
+                    conncheck = True
+                    testednetworkconnector = retrievedconnector
+
+                Else
+                    conncheck = False
+                End If
+
+            End Using
+
+        Catch ex As Exception
+            conncheck = False
+
+        Finally
+            con.Close()
+        End Try
+
+        Return retrievedconnector
+
+    End Function
+
+
+    'Routine to insert the plaintext and cipher text to sql database
+    Public Sub inferclass()
+
+        Dim comstr As String = testednetworkconnector
+        Dim conn As New SqlConnection
+        Dim cmd As New SqlCommand
+        Dim sSQL As String = String.Empty
+
+        Try
+
+
+            conn = New SqlConnection(comstr)
+            conn.Open()
+            cmd.Connection = conn
+            cmd.CommandType = CommandType.Text
+
+            cmd.CommandText = "INSERT INTO dbo.tbl_encryption (  plaintext, ciphertext) Values (  @plaintext, @ciphertext)"
+
+            cmd.Parameters.Add("@plaintext", SqlDbType.NVarChar).Value = txt_plainTest
+            cmd.Parameters.Add("@ciphertext", SqlDbType.NVarChar).Value = txt_encryptedText
+
+            cmd.ExecuteNonQuery()
+            cmd.Parameters.Clear()
+
+        Catch ex As Exception
+
+            MsgBox(" error Inserting to db", ex.Message)
+
+        Finally
+            conn.Close()
+        End Try
+
+    End Sub
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        inferclass()
+    End Sub
+
+
+
+    'function to encrypt the text
+    Function encrypt(ByVal key As String, ByVal plaintext As String)
+        Dim cipherText As String = ""
+        Dim currpos As Integer = 0
+        Dim length, skip As Integer
+
+        Do
+            length = plaintext.Length()
+
+
+            skip = CInt(Math.Ceiling(Rnd() * 3))
+
+            currpos += skip
+
+            cipherText = plaintext.Substring(0, currpos) + key + plaintext.Substring(currpos)
+            currpos += key.Length()
+
+            plaintext = cipherText
+
+        Loop While (currpos < (length - 1))
+
+        Return plaintext
+    End Function
+    Private Sub btn_encrypt_Click(sender As Object, e As EventArgs) Handles btn_encrypt.Click
+        Dim plainText As String = txt_plainTest.Text
+        txt_encryptedText.Text = encrypt("PIX", plainText)
+    End Sub
+    'function to decrypt text
+    Function decrypt(ByVal decryptedtext As String)
+        Dim ciphertext As String = txt_encryptedText.Text
+
+        decryptedtext = ciphertext.Replace("PIX", "")
+        Return decryptedtext
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        txt_decryptedtext.Text = decrypt(txt_encryptedText.Text)
+    End Sub
+End Class
